b164659671b2d85a5f37407c1d25fd5eebe6b2c1
yongchao_huang <rainrow@live.com>
2018-09-07 00:43:14 +0800

    [windy] 登录和家庭页面设计

--- server/app/baseinfo_mod/api/expense_api.py
+++ server/app/baseinfo_mod/api/expense_api.py
@@ -3,3 +3,15 @@ from app.baseinfo_mod.model.expense import Expense
 def create_expense_blueprint(api_manager):    
     expense_api = api_manager.create_api_blueprint(Expense, methods = ['GET', 'POST'])
     api_manager.app.register_blueprint(expense_api, url_prefix = '/api')
+
+def statistics_year_by_family():
+    pass
+
+def statistics_month_by_family():
+    pass
+
+def statistics_year_by_customer():
+    pass
+
+def statistics_month_by_custoer():
+    pass
